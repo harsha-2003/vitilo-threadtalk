9d7bc9427a369a789d098deb0c81930a8540cede
SRAVYA3009 <vallurisrisravya@gmail.com>
2026-02-17 23:33:11 -0500

    Initialize Angular project routing-51

---
+++ .angular/cache/21.1.4/frontend/vite/deps/@angular_cdk_bidi.js
@@ -0,0 +1,14 @@
+import {
+  BidiModule,
+  DIR_DOCUMENT,
+  Dir,
+  Directionality
+} from "./chunk-WZAGPHJU.js";
+import "./chunk-QFF6SDYS.js";
+import "./chunk-PJVWDKLX.js";
+export {
+  BidiModule,
+  DIR_DOCUMENT,
+  Dir,
+  Directionality
+};
